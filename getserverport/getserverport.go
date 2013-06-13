10bfa2c6389498db5a69f596ab029a69758d073f
James Andariese <james.andariese@locationlabs.com>
2013-06-13 15:57:35 -0700

    updated getserverport.go to match the updated port retrieval code

--- getserverport/getserverport.go
+++ getserverport/getserverport.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/jamesandariese/tomcat_info"
+	"github.com/jamesandariese/easy_error"
 	"flag"
 )
 
 var serverdir = flag.String("d", ".", "Path to server (conf/server.xml should exist in this dir)")
 func main() {
+	defer func() {
+		err := easy_error.Apply(recover())
+		if err != nil {
+			fmt.Println(err)
+		}
+	}()
 	flag.Parse()
-	fmt.Println(tomcat_info.GetServerHttpPort(*serverdir + "/conf/server.xml"))
+	server := easy_error.Wrap(tomcat_info.ReadServer(*serverdir)).(*tomcat_info.Server)
+	port := easy_error.Wrap(server.GetHttpPort()).(int16)
+	fmt.Println(port)
 }
